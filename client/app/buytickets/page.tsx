b18eacbe6472c0fa3b3349590b8bce1b2f4dd14b
TPGCIG <tristan.green.300604@gmail.com>
2025-11-15 08:10:12 +1000

    added navbar and search for events

--- client/app/buytickets/page.tsx
+++ client/app/buytickets/page.tsx
@@ -1,6 +1,9 @@
+import { SearchForEvent } from '@/app/buytickets/searchbar'
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-neutral-900 font-sans dark:bg-black">
+        <SearchForEvent/>
       
 
     </div>
