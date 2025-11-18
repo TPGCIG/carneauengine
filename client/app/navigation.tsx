473b41b8f3320ca1c27964877b450a29b094a282
TPGCIG <tristan.green.300604@gmail.com>
2025-11-18 17:21:17 +1000

    style changes in globals and layout, minor edits to navigation bar links

--- client/app/navigation.tsx
+++ client/app/navigation.tsx
@@ -19,33 +19,35 @@ import Link from "next/link"
 export function Navigation() {
 
     return (
-        <div className="w-full">
-        <NavigationMenu className="relative mx-auto rounded-full">
-            <NavigationMenuList className="flex-wrap rounded-full">
-                <NavigationMenuItem className="rounded-full">
-                    <div className="rounded-full">
-                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                        <Link href="/buytickets">Buy Tickets</Link>
-                    </NavigationMenuLink></div>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                        <Link href="/hosting">Host an Event</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                        <Link href="/firstevent">Big First Event</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                        <Link href="/docs">Docs</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
-        </div>
+        <>
+            <div className="w-full fixed">
+            <NavigationMenu className="relative mx-auto rounded-full">
+                <NavigationMenuList className="flex-wrap rounded-full">
+                    <NavigationMenuItem className="rounded-full">
+                        <div className="rounded-full">
+                        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                            <Link href="/events">Buy Tickets</Link>
+                        </NavigationMenuLink></div>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                            <Link href="/hosting">Host an Event</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                            <Link href="/firstevent">Big First Event</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                            <Link href="/docs">Docs</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+            </div><div className="h-16" />
+        </>
     )
 
 };
